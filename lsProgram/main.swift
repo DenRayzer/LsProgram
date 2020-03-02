fbb0e12b26dd8c7cfd37ab686a9130927c9aa920
Елизавета <liz@MacBook-Pro-Elizaveta.local>
2020-03-02 12:51:46 +0300

    commit

---
+++ lsProgram/main.swift
@@ -0,0 +1,46 @@
+//
+//  main.swift
+//  lsProgram
+//
+//  Created by Елизавета on 02.03.2020.
+//  Copyright © 2020 Елизавета. All rights reserved.
+//
+
+import Foundation
+
+var a = readLine()?.split(separator: " ")
+var isHiddenFilesSkip = true
+
+if a?.count == 1 { a?.append(" ") }
+if a?[1] == "-a" { isHiddenFilesSkip = false }
+
+extension FileManager {
+    func urls(for directory: FileManager.SearchPathDirectory, skipsHiddenFiles: Bool = isHiddenFilesSkip ) -> [URL]? {
+     //   var j = FileManager.default.currentDirectoryPath
+        let documentsURL = NSURL(fileURLWithPath: FileManager.default.currentDirectoryPath)
+       
+        let fileURLs = try? contentsOfDirectory(at: documentsURL as URL, includingPropertiesForKeys: nil, options: skipsHiddenFiles ? .skipsHiddenFiles : [] )
+        
+        return fileURLs
+    }
+}
+
+if a?[1] != "-l" {
+    for i in FileManager.default.urls(for: .documentDirectory)! { print(i.lastPathComponent) }
+}
+
+if a?[1] == "-l" {
+    for i in FileManager.default.urls(for: .documentDirectory)! {
+        let fileURL: URL = i
+        let attributes = try FileManager.default.attributesOfItem(atPath: fileURL.path)
+        
+        print("""
+            \(i.lastPathComponent)
+            OwnerAccountName:  \(attributes[.ownerAccountName]!)
+            FileSize:          \(attributes[.size]!)
+            CreationDate:      \(attributes[.creationDate]!)
+            
+            ----------------------
+            
+            """) }
+}
